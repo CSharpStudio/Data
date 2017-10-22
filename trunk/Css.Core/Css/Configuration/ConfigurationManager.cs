a98cf3ceb47c83f2a99fb669c0e6942d3cf2f22e
Eric Leung <ap0310116@163.com>
2017-10-22 23:31:26 +0800

    ConfigurationManager

---
+++ trunk/Css.Core/Css/Configuration/ConfigurationManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Text;
+
+namespace Css.Configuration
+{
+    public static class ConfigurationManager
+    {
+        static IConfiguration _config;
+
+        public static NameValueCollection AppSettings { get; }
+
+        public static NameValueCollection ConnectionStrings { get; }
+    }
+}
---
+++ trunk/Css.Core/Css/Configuration/IConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Css.Configuration
+{
+    public interface IConfiguration
+    {
+        void Load();
+        void Save();
+
+    }
+}
